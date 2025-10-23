e3997934752a00cde83e7b5a1350fbc64b4a93ad
Dan-plus <danielgena88@gmail.com>
2025-10-23 03:24:38 -0500

    db

---
+++ backend/db/dbconfig.js
@@ -0,0 +1,25 @@
+const mysql2 = require('mysql2');
+
+// require('dotenv').config();
+
+// Create a connection pool
+const dbconnect = mysql2.createPool({
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database,
+    port:process.env.port,
+    connectionLimit: 10
+});
+// Export promise-based pool
+module.exports = dbconnect.promise();
+
+// Test connection
+// dbconnect.getConnection((err, connection) => {
+//     if (err) {
+//         console.error("DB connection failed:", err);
+//     } else {
+//         console.log("DB connected successfully!");
+//         connection.release();
+//     }
+// });
