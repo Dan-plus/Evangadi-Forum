0e4a83fdc59721a627da73283813748d64d1c880
Dan-plus <danielgena88@gmail.com>
2025-10-23 03:32:56 -0500

    axios

---
+++ frontend/src/axios/axios.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "http://localhost:5500/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+
+// export default axios.create({
+//   baseURL: "https://qabackend-nnnx.onrender.com/api",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
