15e7111b54130d63277d078fa97a7137c0dba341
Dan-plus <danielgena88@gmail.com>
2025-10-25 22:34:21 -0500

    lastt

--- frontend/src/axios/axios.js
+++ frontend/src/axios/axios.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
-export default axios.create({
-  baseURL: "http://localhost:5500/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
-
-
 // export default axios.create({
-//   baseURL: "https://qabackend-nnnx.onrender.com/api",
+//   baseURL: "http://localhost:5500/api",
 //   headers: {
 //     "Content-Type": "application/json",
 //   },
 // });
+
+
+export default axios.create({
+  baseURL: "https://evangadi-forum-j4d9.onrender.com",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
